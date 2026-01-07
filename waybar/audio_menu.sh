a63f34d10d97627b0d3f371c780f12a0e913a463
Marcin Saffenreuther <theevilone45@gmail.com>
2026-01-07 22:23:55 +0100

    [feature] custom audio pill, switching sinks, adjusting volume

--- waybar/audio_menu.sh
+++ waybar/audio_menu.sh
@@ -3,35 +3,40 @@
 # List all sinks
 sinks=($(pactl list short sinks | awk '{print $2}'))
 
-# Get sink descriptions
-sink_descs=()
-for s in "${sinks[@]}"; do
-    desc=$(pactl list sinks | awk -v sink="$s" '/^Sink #/ {found=0} $0 ~ sink {found=1} found && /Description:/ {print substr($0, index($0,$3)); exit}')
-    sink_descs+=("$desc")
-done
+# Get devices names
+mapfile -t devices < <(
+    pactl list sinks | awk -F'"' '/device.description =/ { print $2 }'
+)
 
-# Get default sink
-default_sink=$(pactl get-default-sink)
+current_sink=$(pactl get-default-sink)
 
-# Build menu
 options=""
 for i in "${!sinks[@]}"; do
     mark=""
-    [[ "${sinks[$i]}" == "$default_sink" ]] && mark="[current] "
-    options+="$mark${sink_descs[$i]}\n"
+    if [[ "${sinks[$i]}" == "$current_sink" ]]; then
+        mark="🟢"
+    fi
+    options+="$mark ${devices[$i]}\n"
 done
-options+="────────────\nCancel"
 
 chosen=$(echo -e "$options" | rofi -dmenu -p "Select Audio Output" \
     -theme-str 'window { location: northeast; anchor: northeast; x-offset: -10px; y-offset: 10px; width: 350px; }' \
-    -theme-str 'listview { lines: 10; }' \
+    -theme-str 'listview { lines: '${#sinks[@]}'; }' \
     -theme-str 'inputbar { enabled: false; }' \
     -theme-str 'mainbox { children: [ listview ]; }')
 
-# Set sink if selected
-for i in "${!sink_descs[@]}"; do
-    if [[ "$chosen" == *"${sink_descs[$i]}"* ]]; then
+for i in "${!devices[@]}"; do
+    if [[ "$chosen" == *"${devices[$i]}"* ]]; then
+        echo "Setting default sink to ${sinks[$i]}"
         pactl set-default-sink "${sinks[$i]}"
+        
+        # move all existing sink inputs to the new sink
+        pactl list short sink-inputs | while read -r stream; do
+            stream_id=$(echo "$stream" | cut -f1)
+            pactl move-sink-input "$stream_id" "${sinks[$i]}" 2>/dev/null
+        done
+        
+        pkill -RTMIN+8 waybar
         exit 0
     fi
 done
